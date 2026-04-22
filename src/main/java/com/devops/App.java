a88f197f76222fbfb59e5e71608f72e34f3fa72b
Ayush <ayushk0081@gmail.com>
2026-04-23 00:44:46 +0530

    Updated all files

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -4,4 +4,8 @@ public class App {
     public String greet() {
         return "CI/CD Pipeline Successful";
     }
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println(app.greet());
+    }
 }
